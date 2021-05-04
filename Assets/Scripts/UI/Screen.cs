99d8a985ed7455f900be8d2bbc54d7aea294ac31
ShiningBreaD <bordakovvbros@gmail.com>
2021-05-04 15:36:38 +0900

    Application template
    Added UI - Main menu, buttons, panels

---
+++ Assets/Scripts/UI/Screen.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Screen : MonoBehaviour
+{
+    [SerializeField] private Animator Animator;
+
+    public void DisableObject()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ChangeAnimationState()
+    {
+        if (gameObject.activeSelf == false)
+        {
+            gameObject.SetActive(true);
+            Animator.SetBool("IsShown", true);
+        }
+        else
+            Animator.SetBool("IsShown", false);
+    }
+}
